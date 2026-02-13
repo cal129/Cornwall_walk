1348cc688cf76caa6a22d48b7654e5a2ca89c517
Callum <callum@Cals-MacBook-Pro-3.local>
2026-02-13 10:08:45 +0000

    update model and admin panel

--- post_walk/admin.py
+++ post_walk/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from post_walk.models import postwalk
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(postwalk)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'authorised')
+    search_fields = ['title']
+    list_filter = ('authorised',)
+    prepopulated_fields = {'slug': ('title',)}
--- post_walk/models.py
+++ post_walk/models.py
@@ -1,20 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
 
 class postwalk(models.Model):
-    walk_id = models.IntegerField(primary_key=True)
+    WALK_TYPES = [
+        (0, 'Circular'),
+        (1, 'Out and Back'),
+        (2, 'One Way'),
+    ]
+    
+    DIFFICULTY_CHOICES = [
+        (0, 'Easy'),
+        (1, 'Moderate'),
+        (2, 'Hard'),
+        (3, 'Expert'),
+    ]
+
+    walk_id = models.AutoField(primary_key=True, editable=False, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='walks')
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
-    difficulty = models.IntegerField()
-    type = models.IntegerField()
-    distance = models.FloatField()
-    time = models.DurationField()
+    difficulty = models.IntegerField(choices=DIFFICULTY_CHOICES, default=0)
+    type = models.IntegerField(choices=WALK_TYPES)
+    distance = models.FloatField(validators=[MinValueValidator(0)])
+    time_hours = models.IntegerField(validators=[MinValueValidator(0)])
+    time_minutes = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(59)])
     photo = CloudinaryField('image', default='placeholder')
     location = models.CharField(max_length=200)
     coordinates = models.CharField(max_length=200)
