43891dc82bf1770e181dd742aa8e86acfc47b2ed
Callum <callum@Cals-MacBook-Pro-3.local>
2026-02-13 10:16:57 +0000

    fix error in setting.py

--- cornish_walks/settings.py
+++ cornish_walks/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
-
+# DEBUG = "DEVELOPMENT" in os.environ
+DEPUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', ]
 
