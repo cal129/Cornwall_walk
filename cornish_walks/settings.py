b8523abd012794e53653f2a8a9803d563b9ac872
Callum <callum@Cals-MacBook-Pro-3.local>
2026-02-13 11:25:39 +0000

    fix database

--- cornish_walks/settings.py
+++ cornish_walks/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = "DEVELOPMENT" in os.environ
@@ -96,12 +96,9 @@ WSGI_APPLICATION = 'cornish_walks.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
-
-
 DATABASES = {
-    'default':
-        dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
