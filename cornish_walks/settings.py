0d8d2cc62aee0661f9b11237c249055a2c5f3925
Callum <callum@Cals-MacBook-Pro-3.local>
2026-02-13 08:06:45 +0000

    add cloudinary

--- cornish_walks/settings.py
+++ cornish_walks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = "DEVELOPMENT" in os.environ
 
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', ]
 
 
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
@@ -44,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'post_walk',
 ]
 
@@ -85,12 +89,18 @@ WSGI_APPLICATION = 'cornish_walks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
 
 
 # Password validation
--- post_walk/models.py
+++ post_walk/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class postwalk(models.Model):
+    walk_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='walks')
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    difficulty = models.IntegerField()
+    type = models.IntegerField()
+    distance = models.FloatField()
+    time = models.DurationField()
+    photo = CloudinaryField('image', default='placeholder')
+    location = models.CharField(max_length=200)
+    coordinates = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    authorised = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
